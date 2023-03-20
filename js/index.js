ce79b185c25bd31d9a7e26af0b163e7e99be8422
Chilmakuri Narasimha sai <74558094+Narasimha13009@users.noreply.github.com>
2023-03-20 13:32:16 +0530

    first commit

---
+++ js/index.js
@@ -0,0 +1,29 @@
+// let myCarousel = document.querySelector("#carousel-button-icon");
+// var state = document.getElementsByClassName("fa fa-play")[0];
+// let loginButton = document.querySelector("#loginButton");
+
+// //have to work on pasue and play of coursel
+// //almost close
+// function pauseFunction() {
+//   var carousel = document.querySelector("#myCarousel");
+//   if (state === document.getElementsByClassName("fa fa-play")[0]) {
+//     document.getElementsByClassName("fa fa-play")[0].className = "fa fa-pause";
+//     state = document.getElementsByClassName("fa fa-pause")[0];
+//     carousel = new bootstrap.Carousel(carousel, {
+//       pause: "onclick",
+//     });
+//   } else if (state === document.getElementsByClassName("fa fa-pause")[0]) {
+//     document.getElementsByClassName("fa fa-pause")[0].className = "fa fa-play";
+//     state = document.getElementsByClassName("fa fa-play")[0];
+//     carousel = new bootstrap.Carousel(carousel, {
+//       pause: "False",
+//     });
+//   }
+// }
+
+// // have to buid the function for login modal
+// // using
+
+// // function loginFunction() {
+// //   loginButton.setAttribute(`${data-bs-toggle:"modal"}`)
+// // }
